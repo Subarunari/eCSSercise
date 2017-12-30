d3190317dc5defe951dc7922348a76c584edf9f1
Subarunari <koyadouhu@gmail.com>
2017-12-30 22:43:26 +0900

    add configuration file

---
+++ config/development.py
@@ -0,0 +1,12 @@
+DATABASE = {
+    'mysql': {
+        'driver': 'mysql',
+        'host': 'localhost',
+        'database': 'ecssercise',
+        'user': 'root',
+        'password': '',
+        'prefix': ''
+    }
+}
+
+OAUTH_CALLBACK_DOMAIN = "http://127.0.0.1:5000"
